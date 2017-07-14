429efc83dd0ebf4b02afd73b4e5458df3fb01d43
Paul Glass <paul.glass@rackspace.com>
2017-07-14 16:01:51 -0500

    Split up heroku bits

--- heroku_start.py
+++ heroku_start.py
@@ -1,48 +1,14 @@
-from datetime import datetime
-import logging
-import os
 import time
+import os
 
-from psn_api.Auth import Auth
-from psn_api.Friend import Friend
-from psn_api.User import User
-from psn_api.Messaging import Messaging
+from check import do_check
 
-PSN_USERNAME = os.environ['PSN_USERNAME']
-PSN_PASSWORD = os.environ['PSN_PASSWORD']
 INTERVAL = int(os.environ['PSN_CHECK_INTERVAL'])
-
-def current_time():
-    return datetime.utcnow().isoformat()
-
-def do_check():
-    auth = Auth(PSN_USERNAME, PSN_PASSWORD)
-    tokens = auth.get_tokens()
-
-    friend = Friend(tokens)
-    online_friends = friend.my_friends(filter='online')['profiles']
-
-    print(current_time())
-    print('%s friends online' % len(online_friends))
-
-    for profile in online_friends:
-        friend = profile['onlineId']
-        status = profile['primaryOnlineStatus']
-        game = profile['presences'][0].get('titleName', '<nothing>')
-        platform = profile['presences'][0].get('platform', '<unknown>')
-        print('[{platform}] {friend} is {status}, playing {game}'.format(
-            platform=platform,
-            friend=friend,
-            status=status,
-            game=game,
-        ))
-
 
 def main():
     print('heroku_start.py - main()')
     while True:
         do_check()
         time.sleep(INTERVAL)
-
 
 main()
